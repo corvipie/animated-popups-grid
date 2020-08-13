6dde8ef6b53f66a6b1df4fce602a5b3d5a584b35
Anton Straka <as@love-it.at>
2020-08-13 02:10:20 +0200

    Restructure jQuery-functions; add constants

--- index.php
+++ index.php
@@ -6,48 +6,99 @@
     <script>
         jQuery(document).ready(function ($) {
 
-            const activateItem = function (item) {
-                let top_position = item.offset().top;
-                let left_position = item.offset().left;
-                let bottom_position = window.innerHeight - (top_position + item.outerHeight());
-                let right_position = window.innerWidth - (left_position + item.outerWidth());
+            let gridItemHandler = (function () {
 
-                let item_id = item.data('item');
-                let active_item = $('#page-overlay .item[data-item=' + item_id + ']');
+                // variables
+                const ITEM_CONTAINER_ID = '#item-container';
+                const PANEL_CONTAINER_ID = '#panel-container';
 
-                let styles = {
-                    top: top_position,
-                    right: right_position,
-                    bottom: bottom_position,
-                    left: left_position
-                };
+                // functions
+                const activatePanel = function (item) {
+                    let item_id = getElementDataId(item);
+                    let active_panel = $(PANEL_CONTAINER_ID + ' .item[data-item=' + item_id + ']');
+                    let item_positions = getItemPositions(item);
 
-                $('#page-overlay').show();
-                active_item.css(styles);
-                active_item.show();
+                    showPanel(active_panel, item_positions);
+                    setTimeout(expandPanel, 10, active_panel);
 
-                setTimeout(
-                    function (active_item) {
-                        let styles = {
-                            top: 0,
-                            right: 0,
-                            bottom: 0,
-                            left: 0
-                        };
-                        active_item.css(styles);
-                    }, 10, active_item);
-
-                if (item.attr('id') !== undefined && item.attr('id') !== "") {
-                    window.location.hash = '#' + item.attr('id');
+                    if (item.attr('id') !== undefined && item.attr('id') !== "" && window.location.hash != '#' + item.attr('id')) {
+                        window.location.hash = '#' + item.attr('id');
+                    }
                 }
-            }
 
-            const deactivateItem = function (active_item) {
-                window.location.hash = '';
+                const deactivatePanel = function (active_panel) {
+                    let panel_id = getElementDataId(active_panel);
+                    let active_item = $(ITEM_CONTAINER_ID + ' .item[data-item=' + panel_id + ']');
+                    let item_positions = getItemPositions(active_item);
 
-                $('#page-overlay').hide();
-                $('#page-overlay .item').hide();
-            }
+                    collapsePanel(active_panel, item_positions);
+                    setTimeout(hidePanel, 1000);
+
+                    window.location.hash = '';
+                }
+
+                const getElementDataId = function (item) {
+                    return item.data('item');
+                }
+
+                const getItemPositions = function (item) {
+                    let top_position = item.offset().top;
+                    let left_position = item.offset().left;
+                    let bottom_position = window.innerHeight - (top_position + item.outerHeight());
+                    let right_position = window.innerWidth - (left_position + item.outerWidth());
+
+                    return {
+                        top: top_position,
+                        right: right_position,
+                        bottom: bottom_position,
+                        left: left_position
+                    };
+                }
+
+                const showPanel = function (active_panel, item_positions) {
+                    $(PANEL_CONTAINER_ID).show();
+                    active_panel.css(item_positions);
+                    active_panel.show();
+                }
+
+                const hidePanel = function () {
+                    $(PANEL_CONTAINER_ID).hide();
+                    $(PANEL_CONTAINER_ID).children().hide();
+                }
+
+                const expandPanel = function (active_panel) {
+                    let expanded_panel_positions = {
+                        top: 0,
+                        right: 0,
+                        bottom: 0,
+                        left: 0
+                    };
+                    active_panel.css(expanded_panel_positions);
+                }
+
+                const collapsePanel = function (active_panel, item_positions) {
+                    active_panel.css(item_positions);
+                }
+
+                // Public API
+                return {
+                    activatePanel: activatePanel,
+                    deactivatePanel: deactivatePanel,
+                };
+            })();
+
+
+            $('.item').on('click', function () {
+                if ($(this).parent().hasClass('container')) {
+                    let clicked_item = $(this);
+                    gridItemHandler.activatePanel(clicked_item);
+                }
+            });
+
+            $('.close-pop-up').on('click', function () {
+                let active_panel = $(this).parent();
+                gridItemHandler.deactivatePanel(active_panel);
+            });
 
             if (location.hash && location.hash.length) {
                 var hash = decodeURIComponent(location.hash.substr(1));
@@ -58,22 +109,81 @@
                     scrollTop: $('#' + hash).offset().top
                 });
 
-                let obj = $('#' + hash);
-                activateItem(obj);
+                let clicked_item = $('#' + hash);
+                gridItemHandler.activatePanel(clicked_item)
             }
 
+            /*
+                        const activateItem = function (item) {
+                            let top_position = item.offset().top;
+                            let left_position = item.offset().left;
+                            let bottom_position = window.innerHeight - (top_position + item.outerHeight());
+                            let right_position = window.innerWidth - (left_position + item.outerWidth());
 
-            $('.item').on('click', function () {
-                if ($(this).parent().hasClass('container')) {
-                    let obj = $(this);
-                    activateItem(obj);
-                }
-            });
+                            let item_id = item.data('item');
+                            let active_item = $('#page-overlay .item[data-item=' + item_id + ']');
 
-            $('.close-pop-up').on('click', function () {
-                let active_item = $(this);
-                deactivateItem(active_item);
-            });
+                            let styles = {
+                                top: top_position,
+                                right: right_position,
+                                bottom: bottom_position,
+                                left: left_position
+                            };
+
+                            $('#page-overlay').show();
+                            active_item.css(styles);
+                            active_item.show();
+
+                            setTimeout(
+                                function (active_item) {
+                                    let styles = {
+                                        top: 0,
+                                        right: 0,
+                                        bottom: 0,
+                                        left: 0
+                                    };
+                                    active_item.css(styles);
+                                }, 10, active_item);
+
+                            if (item.attr('id') !== undefined && item.attr('id') !== "") {
+                                window.location.hash = '#' + item.attr('id');
+                            }
+                        }
+
+                        const deactivateItem = function (active_item) {
+                            window.location.hash = '';
+
+                            $('#page-overlay').hide();
+                            $('#page-overlay .item').hide();
+                        }
+
+                        if (location.hash && location.hash.length) {
+                            var hash = decodeURIComponent(location.hash.substr(1));
+
+                            // Using jQuery's animate() method to add smooth page scroll
+                            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+                            $('html, body').animate({
+                                scrollTop: $('#' + hash).offset().top
+                            });
+
+                            let obj = $('#' + hash);
+                            activateItem(obj);
+                        }
+
+
+                        $('.item').on('click', function () {
+                            if ($(this).parent().hasClass('container')) {
+                                let obj = $(this);
+                                activateItem(obj);
+                            }
+                        });
+
+                        $('.close-pop-up').on('click', function () {
+                            let active_item = $(this);
+                            deactivateItem(active_item);
+                        });
+
+             */
         });
     </script>
 
@@ -115,20 +225,20 @@
             background-color: purple;
         }
 
-        #page-overlay {
+        #panel-container {
             position: fixed;
             top: 0;
             right: 0;
             bottom: 0;
             left: 0;
             display: none;
-        / / background-color: rgba(0, 0, 0, 0.4);
+            //background-color: rgba(0, 0, 0, 0.8);
         }
 
-        #page-overlay .item {
+        #panel-container .item {
             display: none;
             position: absolute;
-        / / background-color: rgba(255, 255, 255, 0.8);
+            //background-color: rgba(255, 255, 255, 0.8);
             transition: all 1s;
         }
     </style>
@@ -137,14 +247,14 @@
 </head>
 <body>
 
-<section class="container">
+<section id="item-container" class="container">
     <div id="erste-kachel" data-item="1" class="item item-a">one</div>
     <div id="zweite-kachel" data-item="2" class="item item-b">two</div>
     <div id="plumpsklo" data-item="3" class="item item-c">three</div>
     <div id="oma" data-item="4" class="item item-d">four</div>
 </section>
 
-<div id="page-overlay">
+<div id="panel-container">
     <div data-item="1" class="item item-a">one active!!
         <div class="close-pop-up">SCHLIESSEN X</div>
     </div>
